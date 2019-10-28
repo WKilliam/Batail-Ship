e782e987a6ea7cd6cf39127e3e2a960d6d2c3a4c
teddy <william.k97430@gmail.com>
2019-10-28 11:34:11 +0100

    Tableau(test)

--- BSHiP/src/com/company/Main.java
+++ BSHiP/src/com/company/Main.java
@@ -3,6 +3,33 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
     }
+}
+/* class TableauDouble {
+   public static void main(String[] arg) {
+      int[][] tableau;
+      int i, j;
+
+      tableau = new int[2][3];
+      for (int i = 0;i < tableau.length; i++) {
+         for (int j = 0;j < tableau[i].length;j++) {
+            tableau[i][j] = i + j;
+            System.out.print(tableau[i][j] + " ");
+         }
+         System.out.println();
+      }
+      System.out.println();
+
+    tableau = new int[3][];
+    for (i = 0; i < tableau.length; i++) {
+	tableau[i] = new int[i + 1];
+	for (j = 0; j < tableau[i].length; j++) {
+	    tableau[i][j] = i + j;
+            System.out.print(tableau[i][j] + " ");
+         }
+         System.out.println();
+      }
+      System.out.println();
+   }
 }
