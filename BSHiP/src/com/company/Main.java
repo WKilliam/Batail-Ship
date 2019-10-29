9753f2f6e915c9c8c5242de0724ad02fb25d235c
cassel <casselboris92@gmail.com>
2019-10-29 10:20:39 +0100

    Fonction Init et nettoyage

--- BSHiP/src/com/company/Main.java
+++ BSHiP/src/com/company/Main.java
@@ -19,8 +19,19 @@ public class Main {
             }
         }*/
 
+// init
+    public static void FonctionInit(char tableau[][]) {
 
+        int n = tableau.length;
+        int m = tableau[0].length;
+        int row;
+        for (row=0;row<n;row=row+1) {
+            int col;
+            for (col = 0; col < m; col = col + 1) {
+                tableau[row][col] = '~';
+            }
 
+        }   }
 
 
 // print the table
@@ -47,77 +58,22 @@ public class Main {
         }
 
     }
-        /*int x;
-        int yy;
-        int y = 1;
-        for (x = 1; x <= 10; x = x + 1){
-            System.out.println("X");
-            for (yy=2;yy <=9 ; yy=yy+1){
-                System.out.println(y* 10);*/
-
-
-           /* //System.out.println("X");
-            for (y = 1; y <= 10; y = y + 1) {
-                System.out.println("Y");*/
 
 
     public static void main(String[] args) {
 
+
         char tab1[][]= new char [10][10];
 
+        FonctionInit(tab1);
         FonctionAffichageX(tab1);
 
-        /*
-        int Tableau[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-        char tableauCaractere[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
-
-        for (int i = 0; i < tableauCaractere.length; i++) {
-            System.out.println("À l'emplacement " + i + " du tableau nous avons = " + tableauCaractere[i]);
-        }*/
     }
 }
 
 
-        /*FonctionAffichageX();*
-
-
         
-        /*
-        class Fenetre extends JFrame {
-            public Fenetre() {
 
-                JFrame fenetre = new JFrame();
-
-                //Définit un titre pour notre fenêtre
-                fenetre.setTitle("Bataille Navale");
-
-                //Définit sa taille : 500 pixels de large et 500 pixels de haut
-                fenetre.setSize(500, 500);
-
-                //Nous demandons maintenant à notre objet de se positionner au centre
-                fenetre.setLocationRelativeTo(null);
-
-                //Termine le processus lorsqu'on clique sur la croix rouge
-                fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-                //Et enfin, la rendre visible
-                fenetre.setVisible(true);
-
-                //Instanciation d'un objet JPanel
-                JPanel pan = new JPanel();
-
-                //Définition de sa couleur de fond
-                pan.setBackground(Color.ORANGE);
-
-                //On prévient notre JFrame que notre JPanel sera son content pane
-                this.setContentPane(pan);
-                this.setVisible(true);
-
-
-            }
-        }
-*/
 
 
 
